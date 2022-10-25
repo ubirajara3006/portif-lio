681a8ad7bb1ebb57d37e5a5d285fff495113b2a1
ubirajara3006 <110038008+ubirajara3006@users.noreply.github.com>
2022-10-25 19:06:26 -0300

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -6,6 +6,23 @@ const iconMenu = document.getElementById('icon-menu')
 const titulo = document.querySelector('#main-title h1')
 const logo = document.querySelector('nav img')
 const cover = document.querySelectorAll('.cover')
+let c = 0;
+
+//Lógica scroll-animation
+function ativaNoScroll(){
+document.querySelectorAll('.fadein').forEach((e, index) => {
+    if(e.getBoundingClientRect().top < window.innerHeight){
+        e.style = 'animation: fadein 2s linear;';
+    };
+})
+document.querySelectorAll('.fadein-img img').forEach((img, index) => {
+    if(img.getBoundingClientRect().top < window.innerHeight){
+       img.src = img.getAttribute('data-src');
+    };
+})
+}
+
+window.addEventListener('scroll', ativaNoScroll);
 
 
 //Lógica recarregar página
@@ -16,31 +33,18 @@ function recarregar(){
     },500)  
 }
 
-//Lógica animação do título
-
-function h1Writer (titulo){
-    let c = 0
-    const textoArray= titulo.innerHTML.split('')
-    titulo.innerHTML = ''
-    const loop = setInterval(()=>{
-    if(c < textoArray.length){
-        titulo.innerHTML += textoArray[c]
-        c++
-    }else{
-        clearInterval(loop)
-    }
-    }, 100)
-}
-h1Writer(titulo)
-
 //Lógica para animação da nav e barra de menu.
 function menu() {
-    if(barraMenu.style.display !='flex'){
+    
+    if(c % 2 == 0){
+        c++
         abrirMenu()
     }else{
+        c++
         fecharMenu()
     } 
 }
+
 function abrirMenu(){
     barraMenu.style.display = 'flex'
     barraMenu.style.animation = 'menuAnimation 0.5s'
@@ -54,8 +58,8 @@ function fecharMenu(){
     iconMenu.style.color = '#0F141E'
     iconMenu.innerHTML = '<span>MENU</span><ion-icon name="menu-outline"></ion-icon>'
     logo.style.filter = 'none'
-    barraMenu.style.animation = 'menuAnimationOFF 0.5s'
+    barraMenu.style.animation = 'menuAnimationOFF 1s'
     setTimeout(() => {
         barraMenu.style.display ='none'
-    },500)
+    },1000)
 }
